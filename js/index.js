24f2af9e868d4538399251ce8149fa431a86f380
Alexei7845 <ga.garciachaire@ugto.mx>
2023-02-23 11:37:19 -0600

    agregamos cars e imagenes

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const cardTop = document.querySelector('#cardTop').content
+const contenido = document.querySelector('#contenido')
+const fragment = document.createDocumentFragment()
+let topTwoHundred = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadMusicData()
+})
+
+const loadMusicData = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd8ecb9a836mshb60510b4e9b9fecp11fe59jsncd93b3509549',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = response
+            creaCards()
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+}
+
+const creaCards = () => {
+    topTwoHundred.forEach((song) => {
+
+        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
+        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+
+        const clone = cardTop.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    contenido.appendChild(fragment)
+}
